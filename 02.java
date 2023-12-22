ba1a3e78cdbafb4d285fc00702f24229c9b627c4
111B15166 <145734173+111B15166@users.noreply.github.com>
2023-12-22 11:43:14 +0800

    02.java

--- 02.java
+++ 02.java
@@ -1,1 +1,20 @@
+import java.io.*;
+
+public class J02 {
+    public static void main(String[] args) {
+        char data[] = new char[128];
+        try {
+            FileReader fr = new FileReader("C:\\新增資料夾\\knowledge is power.txt");
+            int num = fr.read(data);
+            System.out.println("Characters read = " + num);
+
+            String str = new String(data).toUpperCase();
+            System.out.println(str);
+
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
